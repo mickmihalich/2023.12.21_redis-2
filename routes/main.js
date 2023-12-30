7799296fd14ece8869a212804aeb4c2bfec00e99
mickmihalich <mickmihalich@gmail.com>
2023-12-30 17:14:48 +0200

    xz

--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,25 @@
 import express from 'express';
 const router = express.Router();
 
+
+
+
+//connect to Redis
+import { createClient } from 'redis';
+
+const client = await createClient({
+  url: 'redis://default:default@10.10.33.132:6379'  
+})
+  .on('error', err => console.log('Redis Client Error', err))
+  .connect();
+
+await client.set('title', 'BD Redis');
+const value = await client.get('title');
+await client.disconnect();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: value });
 });
 
 export default router;
